5786ab991924c64b38e502f3101c436019a2122f
radumirescu <mirescu.raducu@gmail.com>
2025-07-14 23:19:25 +0300

    Add pilates correct id

--- initial.go
+++ initial.go
@@ -60,7 +60,7 @@ func main() {
 			name:             "Pilates - Marti",
 			bookingWeekDay:   "Monday",
 			bookingStartTime: "17:40",
-			class:            ClassSchedule{410, 733153, 24},
+			class:            ClassSchedule{410, 733157, 29},
 			account:          []Credentials{alina},
 		},
 		Bookings{ // Pilates
